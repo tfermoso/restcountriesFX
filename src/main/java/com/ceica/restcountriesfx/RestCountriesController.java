d8fc5b4982599dfca5d516cf9eabaa1c44ca7684
tfermoso <tomas.fermoso@gmail.com>
2024-02-19 20:10:30 +0100

    Formateando la aplicacion FX

--- src/main/java/com/ceica/restcountriesfx/RestCountriesController.java
+++ src/main/java/com/ceica/restcountriesfx/RestCountriesController.java
@@ -3,7 +3,7 @@ package com.ceica.restcountriesfx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class RestCountriesController {
     @FXML
     private Label welcomeText;
 
