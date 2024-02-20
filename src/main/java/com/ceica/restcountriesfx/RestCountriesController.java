f89d7264958d5793ab810c2e4fe20a8b8d5e952f
tfermoso <tomas.fermoso@gmail.com>
2024-02-20 16:40:18 +0100

    Mostrando las regiones

--- src/main/java/com/ceica/restcountriesfx/RestCountriesController.java
+++ src/main/java/com/ceica/restcountriesfx/RestCountriesController.java
@@ -1,14 +1,40 @@
 package com.ceica.restcountriesfx;
 
+import com.ceica.restcountriesfx.Models.CountryDTO;
+import com.ceica.restcountriesfx.Services.FakeRestCountriesService;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+import java.lang.annotation.Target;
 
 public class RestCountriesController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+  @FXML
+  protected ComboBox comboRegions;
+  @FXML
+    protected TableView<CountryDTO> tblCountries;
+  @FXML
+    protected TableColumn<CountryDTO,String> countryNameColumn;
+  @FXML
+    protected ImageView imgFlag;
+  @FXML
+    protected TextField txtCountryName;
+  @FXML
+    protected TextField txtCountryPopulation;
+  @FXML
+    protected TextField txtCountryCapital;
+  @FXML
+    protected TextField txtCountryCoin;
+@FXML
+  public void initialize(){
+  FakeRestCountriesService fakeRestCountriesService=new FakeRestCountriesService();
+    comboRegions.getItems().addAll(fakeRestCountriesService.getRegions());
+    countryNameColumn.setCellValueFactory(cell->new SimpleStringProperty(cell.getValue().getName()));
+  }
+  @FXML
+    public void btnClear(ActionEvent actionEvent) {
     }
 }
