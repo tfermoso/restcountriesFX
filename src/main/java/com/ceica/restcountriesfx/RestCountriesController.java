e94b3fb131b04181a5e05228be069abe2cc33619
tfermoso <tomas.fermoso@gmail.com>
2024-02-21 18:29:29 +0100

    Mover cursor por la tableView

--- src/main/java/com/ceica/restcountriesfx/RestCountriesController.java
+++ src/main/java/com/ceica/restcountriesfx/RestCountriesController.java
@@ -57,6 +57,16 @@ public class RestCountriesController {
             Image imgen=new Image(countryDTO.getFlag());
             imgFlag.setImage(imgen);
         });
+        tblCountries.setOnKeyPressed(e->{
+            String countrycca3=tblCountries.getSelectionModel().getSelectedItem().getCca3();
+            CountryDTO countryDTO=fakeRestCountriesService.getCountryByCca3(countrycca3);
+            txtCountryName.setText(countryDTO.getName());
+            txtCountryCapital.setText(countryDTO.getCapital());
+            txtCountryCoin.setText(countryDTO.getCoin());
+            txtCountryPopulation.setText(String.valueOf(countryDTO.getPopulation()));
+            Image imgen=new Image(countryDTO.getFlag());
+            imgFlag.setImage(imgen);
+        });
         countryNameColumn.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getName()));
     }
 
